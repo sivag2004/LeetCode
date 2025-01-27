02d5496702d48a3002d380edbd1325763e2819ca
Sivaganesh T <125188412+sivag2004@users.noreply.github.com>
2025-01-27 19:04:42 +0530

    Add files via upload

---
+++ FindallMissingNumber.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+//Asked in Google
+public class FindallMissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> missingNumbers = findDisappearedNumbers(arr);
+        System.out.println(missingNumbers);
+
+    }
+    static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            }else{
+                i++;
+            }
+        }
+        //just find missing number
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if(nums[index] != index+1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
