3786a1896b35903054ed9a4eaa1aa3bd8f553507
Sivaganesh T <125188412+sivag2004@users.noreply.github.com>
2025-01-28 19:45:16 +0530

    FirstAndLastPosition.java

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 public class FirstAndLastPosition {
     public static void main(String[] args) {
         int[] nums = {5, 7, 7, 8, 8, 10};
